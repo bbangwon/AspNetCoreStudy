6a6d71c20b240b08ee46a079fa4e18eca7b4988c
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2021-10-21 03:05:37 +0900

    마무리

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
 
 namespace RazorPagesPizza.Pages
 {
@@ -13,8 +14,34 @@ namespace RazorPagesPizza.Pages
         [BindProperty]
         public Pizza NewPizza { get; set; }
 
+        public List<Pizza> pizzas;
+
         public void OnGet()
         {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
         }
     }
 }
